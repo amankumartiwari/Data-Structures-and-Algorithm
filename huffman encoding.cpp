0affde4f5ede6255375af3f80db52d3186ca01b4
Aman Kumar Tiwari <kumaramantiwari98@gmail.com>
2019-10-16 21:56:23 +0530

    huffman modification

--- huffman encoding.cpp
+++ huffman encoding.cpp
@@ -83,6 +83,18 @@ void HuffmanCodes(char data[], int freq[], int size)
 	printCodes(minHeap.top(), ""); 
 } 
 
+int main() 
+{ 
+
+	char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' }; 
+	int freq[] = { 5, 9, 12, 13, 16, 45 }; 
+
+	int size = sizeof(arr) / sizeof(arr[0]); 
+
+	HuffmanCodes(arr, freq, size); 
+
+	return 0; 
+} 
 // Driver program to test above functions 
 
  

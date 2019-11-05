83a93500f023a4e740d7e870616d1bc4152ca8ce
Aman Kumar Tiwari <kumaramantiwari98@gmail.com>
2019-11-05 17:01:08 +0530

    dijkstra algo

--- Dijkstra Algorithm.cpp
+++ Dijkstra Algorithm.cpp
@@ -6,11 +6,8 @@
  
 #define V 9 
 
-// A utility function to find the vertex with minimum distance value, from 
-// the set of vertices not yet included in shortest path tree 
 int minDistance(int dist[], bool sptSet[]) 
 { 
-	// Initialize min value 
 	int min = INT_MAX, min_index; 
 
 	for (int v = 0; v < V; v++) 
@@ -20,7 +17,6 @@ int minDistance(int dist[], bool sptSet[])
 	return min_index; 
 } 
 
-// A utility function to print the constructed distance array 
 int printSolution(int dist[]) 
 { 
 	printf("Vertex \t\t Distance from Source\n"); 
@@ -28,8 +24,6 @@ int printSolution(int dist[])
 		printf("%d \t\t %d\n", i, dist[i]); 
 } 
 
-// Function that implements Dijkstra's single source shortest path algorithm 
-// for a graph represented using adjacency matrix representation 
 void dijkstra(int graph[V][V], int src) 
 { 
 	int dist[V]; // The output array. dist[i] will hold the shortest 
@@ -69,10 +63,10 @@ void dijkstra(int graph[V][V], int src)
 	printSolution(dist); 
 } 
 
-// driver program to test above function 
+ 
 int main() 
 { 
-	/* Let us create the example graph discussed above */
+	
 	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
 						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 }, 
 						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 }, 
